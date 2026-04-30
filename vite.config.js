51286b6fc2794c0803abd9e530a24d1bee16a8df
Genaro Robles <rqgenaro_junior2001@yahoo.es>
2026-04-30 04:19:23 +0000

    configurador el vite config CORS

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        strictPort: true,
+        cors: true,
+        hmr: {
+            host: "mibodeguita.dev.local",
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
